1d26c5371805fb7df01b638eb7376d5dd5d05ffd
Arnaud Leduc <arnaud.leduc3377@gmail.com>
2023-06-15 17:20:04 +0200

    Created Home, About pages, implementing styling and some functionnalities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import App from './components/App';
-
+import Home from './pages/Home/index';
+import About from './pages/About/index';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );

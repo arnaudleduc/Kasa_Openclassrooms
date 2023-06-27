98d34e6d9d503a8a5202058d60a4a86f04ae96aa
Arnaud Leduc <arnaud.leduc3377@gmail.com>
2023-06-27 12:16:29 +0200

    Added responsive bahaviour on homepage components

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/LOGO.png';
 import whiteLogo from '../assets/LOGO_WHITE.png';
+import '../styles/Logo.css';
 
 function Logo({ logoColor }) {
 
   return (
-    logoColor === 'red' ? <Link to="/"><img src={logo} alt="Logo Kasa" /></Link> : <img className="kasa-footer-logo" src={whiteLogo} alt="Logo Kasa" /> 
+    logoColor === 'red' ?
+      <Link to="/"><img src={logo} alt="Logo Kasa" className="kasa-header-logo" /></Link>
+      :
+      <img className="kasa-footer-logo" src={whiteLogo} alt="Logo Kasa" />
   )
 }
 

5c40023a25ff37d5d0a03c83f0aec7dc9c6ef65b
Arnaud LEDUC <arnaud.leduc@skilleos.com>
2023-06-28 08:08:23 +0200

    Added tablet behaviour on homepage

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -4,7 +4,7 @@ import '../styles/Thumb.css'
 function Thumb({ dataObject }) {
   return (
     <div className="kasa-thumb">
-      <Link to={`/location/${dataObject.id}`}>
+      <Link to={`/location/${dataObject.id}`} className="kasa-thumb-link">
         <span className="kasa-thumb-title">{ dataObject.title }</span>
         <div className="kasa-thumb-overlay"></div>
         <img src={ dataObject.cover } alt="Appartement en location" className="kasa-thumb-img"/>
